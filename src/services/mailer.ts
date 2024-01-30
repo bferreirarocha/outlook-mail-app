3fab91d8869c3d03dc938af29f3b5e2eaad82b45
bferreirarocha <bruno.ferreira.rocha@gmail.com>
2024-01-30 16:51:25 +0100

    Mailer Structure

---
+++ src/services/mailer.ts
@@ -0,0 +1,15 @@
+import { Subscription } from '../types';
+import { Order } from "../types"
+
+export const OrderReceived = (order:Order):void => { 
+  
+}  
+export const PaymentReceived = (order:Order):void => { 
+
+}  
+export const OrderShipped = (order:Order):void => { 
+
+}   
+export const SubscriptionCreated = (subs:Subscription):void => { 
+
+} 
